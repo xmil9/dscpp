e8e7d77b8fdc8cb1d7499896ca2389dc90a2a1a5
Mike Lindner <mlindner9@yahoo.com>
2020-05-27 20:50:01 -0400

    Type traits extension to check if a type is an iterator.

---
+++ TypeTraitsEx.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iterator>
+#include <type_traits>
+
+
+template <typename T, typename = void> inline constexpr bool IsIterator_v = false;
+
+// Will only pass SFINAE, if the given type defines iterator_category.
+template <typename T>
+inline constexpr bool
+   IsIterator_v<T, std::void_t<typename std::iterator_traits<T>::iterator_category>> =
+      true;
+
+template <typename T> struct IsIterator : std::bool_constant<IsIterator_v<T>>
+{
+};
