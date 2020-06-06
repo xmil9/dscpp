3f94cbc7ee60db9999edc04ce880b27f8570996a
Mike Lindner <mlindner9@yahoo.com>
2020-06-06 07:44:21 -0400

    SboVector - Simplify inserting initializer list by calling insert range.

--- SboVector.h
+++ SboVector.h
@@ -824,58 +824,7 @@ template <typename T, std::size_t N>
 typename SboVector<T, N>::iterator SboVector<T, N>::insert(const_iterator pos,
                                                            std::initializer_list<T> ilist)
 {
-   // Cases:
-   // - In buffer and enough capacity to stay in buffer.
-   // - In buffer and not enough capacity. Allocate heap.
-   // - On heap and enough capacity.
-   // - On heap and not enough capacity. Need to reallocate.
-
-   const std::size_t count = ilist.size();
-   const std::size_t posOffset = pos - cbegin();
-   if (count == 0)
-      return begin() + posOffset;
-
-   const std::size_t newSize = size() + count;
-   const bool fitsBuffer = fitsIntoBuffer(newSize);
-   const bool canReuseHeap = onHeap() && m_capacity >= newSize;
-   const bool allocHeap = !fitsBuffer && !canReuseHeap;
-   const std::size_t newCap = allocHeap ? recalcCapacity(newSize) : capacity();
-
-   // Perform allocation up front to prevent inconsistencies if allocation
-   // fails.
-   T* dest = data();
-   if (allocHeap)
-      dest = allocateMem(newCap);
-
-   const std::size_t tailSize = cend() - pos;
-   const std::size_t frontSize = posOffset;
-
-   if (tailSize > 0)
-   {
-      T* tailSrc = data() + posOffset;
-      T* tailDest = dest + posOffset + count;
-      internals::relocateRightOverlapped(tailSrc, tailSrc + tailSize, tailDest);
-   }
-
-   std::uninitialized_copy_n(ilist.begin(), count, dest + posOffset);
-
-   const bool relocateFront = allocHeap;
-   if (frontSize > 0 && relocateFront)
-   {
-      // Relocating the front only happens for reallocations, so we don't need to
-      // worry about overlaps.
-      internals::relocate_n(data(), frontSize, dest);
-   }
-
-   m_size = newSize;
-   if (allocHeap)
-   {
-      deallocate();
-      m_data = dest;
-      m_capacity = newCap;
-   }
-
-   return begin() + posOffset;
+   return insert(pos, ilist.begin(), ilist.end());
 }
 
 
