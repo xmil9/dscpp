90b118193a274024f1f4aed8a02bc35163f04e20
Mike Lindner <mlindner9@yahoo.com>
2023-05-14 18:02:36 -0400

    Convert Heap to HeapView. Add pop() member function.

--- Heap.cpp
+++ Heap.cpp
@@ -14,31 +14,33 @@ namespace ds
 
 ///////////////////
 
-// Heap data structure.
-// A binary tree that fulfills a given heap property for each of its nodes.
-// The heap property is that each parent node fulfills a condition when compared
-// to its two child nodes.
-// Examples for heap types:
-// Max-heap: Each parent node is larger than its child nodes.
-// Min-heap: Each parent node is smaller than its child nodes.
-template <typename T, typename Condition> class Heap
+// HeapView data structure.
+// Does not store elements itself but uses passed storage.
+// Conceptuall builds a binary tree that fulfills a given heap property for each of its
+// nodes. The heap property is that each parent node fulfills a condition when compared to
+// its two child nodes.
+// HeapView types (see type aliases below):
+//  Max-heap: Each parent node is larger than its child nodes.
+//  Min-heap: Each parent node is smaller than its child nodes.
+template <typename T, typename Condition> class HeapView
 {
  public:
    using value_type = T;
    using size_type = size_t;
-   using iterator = std::deque<T>::iterator;
-   using const_iterator = std::deque<T>::const_iterator;
+   using iterator = T*;
 
  public:
-   Heap() = default;
-   Heap(const Condition& heapProp);
-   Heap(std::initializer_list<T> ilist);
-   Heap(std::initializer_list<T> ilist, const Condition& heapProp);
-   template <typename SrcIter> Heap(SrcIter first, SrcIter last);
-   template <typename SrcIter>
-   Heap(SrcIter first, SrcIter last, const Condition& heapProp);
+   HeapView() = default;
+   HeapView(T* elems, size_t numElems, const Condition& heapProp);
 
-   size_t size() const { return m_tree.size(); }
+   size_t size() const { return m_heapSize; }
+   bool empty() const { return m_heapSize == 0; }
+
+   // Returns the top element of the heap.
+   // Internally this also sorts that element at the correct place in the passed
+   // in storage. Calling pop() repeatedly until the heap is empty will sort the
+   // underlying data.
+   const T* pop();
 
  private:
    // 1-based logical heap index.
@@ -58,20 +60,21 @@ template <typename T, typename Condition> class Heap
    // Access element at given heap index.
    T& elem(HeapIdx i) { return m_tree[arrayIdx(i)]; }
 
-   // Builds a heap from a given sequence of elements.
-   template <typename SrcIter> void buildHeap(SrcIter first, SrcIter last);
+   // Builds a heap out of the given element array.
+   void buildHeap();
 
    // Ensures that the binary tree at node i satisfies the heap-property, assuming
    // that the left and right binary subtrees are heaps.
    void heapify(HeapIdx i);
 
  private:
-   // Array representing a binary tree.
+   // Treats the passed data as an array representing a binary tree.
    // The left and right subtrees of the node at array element i are located in
    // the array at positions 2*i and 2*i+1 (assuming one-based indices for easier
    // calculation). A node's parent is located at the array position floor(i/2)
    // (for one-based indices).
-   std::deque<T> m_tree;
+   T* m_tree = nullptr;
+   size_t m_heapSize = 0;
 
    // Condition that defines the heap-property.
    Condition m_heapProp{};
@@ -79,58 +82,43 @@ template <typename T, typename Condition> class Heap
 
 // Standard heap types.
 // Max-heaps are used for the heap sort algorithm.
-template <typename T> using MaxHeap = Heap<T, std::greater<T>>;
+template <typename T> using MaxHeap = HeapView<T, std::greater<T>>;
 // Min-heaps are used to implement priority queue.
-template <typename T> using MinHeap = Heap<T, std::less<T>>;
+template <typename T> using MinHeap = HeapView<T, std::less<T>>;
 
 // Implementation
 
 template <typename T, typename Condition>
-Heap<T, Condition>::Heap(const Condition& heapProp) : m_heapProp{heapProp}
+HeapView<T, Condition>::HeapView(T* elems, size_t numElems, const Condition& heapProp)
+: m_tree{elems}, m_heapSize{numElems}, m_heapProp{heapProp}
 {
+   buildHeap();
 }
 
 template <typename T, typename Condition>
-Heap<T, Condition>::Heap(std::initializer_list<T> ilist)
+const T* HeapView<T, Condition>::pop()
 {
-   buildHeap(std::begin(ilist), std::end(ilist));
+   if (empty())
+      return nullptr;
+
+   std::swap(m_tree[0], m_tree[--m_heapSize]);
+   return &m_tree[m_heapSize];
 }
 
 template <typename T, typename Condition>
-Heap<T, Condition>::Heap(std::initializer_list<T> ilist, const Condition& heapProp)
-: m_heapProp{heapProp}
+void HeapView<T, Condition>::buildHeap()
 {
-   buildHeap(std::begin(ilist), std::end(ilist));
-}
+   if (empty())
+      return;
 
-template <typename T, typename Condition>
-template <typename SrcIter>
-Heap<T, Condition>::Heap(SrcIter first, SrcIter last)
-{
-   buildHeap(first, last);
-}
-
-template <typename T, typename Condition>
-template <typename SrcIter>
-Heap<T, Condition>::Heap(SrcIter first, SrcIter last, const Condition& heapProp)
-: m_heapProp{heapProp}
-{
-   buildHeap(first, last);
-}
-
-template <typename T, typename Condition>
-template <typename SrcIter>
-void Heap<T, Condition>::buildHeap(SrcIter first, SrcIter last)
-{
-   const size_t n = std::distance(first, last);
-   const HeapIdx lastInnerNode = n << 1;
+   const HeapIdx lastInnerNode = size() << 1;
 
    // From bottom to top heapify all non-leaf nodes.
    for (HeapIdx i = lastInnerNode; i >= 1; --i)
       heapify(i);
 }
 
-template <typename T, typename Condition> void Heap<T, Condition>::heapify(HeapIdx i)
+template <typename T, typename Condition> void HeapView<T, Condition>::heapify(HeapIdx i)
 {
    const HeapIdx l = left(i);
    const HeapIdx r = right(i);
